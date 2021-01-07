2e149afaaf267439767d08e56c0baba1485e0f28
I <ivar123@gmail.com>
2021-01-07 09:43:14 -0300

    first commit in a long time

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,14 @@
-#! /bin/sh
-
-# Website commands
-alias generate='rm -f ~/dst/.files && ~/bin/ssg5 src dst "Ivar Castro" "http://ivarcastro.com"'
+# This file is run by all fish instances.
+# To include configuration only for login shells, use
+# if status --is-login
+#    ...
+# end
+# To include configuration only for interactive shells, use
+# if status --is-interactive
+#   ...
+# end
+if status --is-interactive
+  abbr --add --global   daymode 'printf \'\x1b]10;black\\x1b\\\\\\\\\x1b]11;white\x1b\\\\\''
+  abbr --add --global nightmode 'printf \'\x1b]10;white\\x1b\\\\\\\\\x1b]11;black\x1b\\\\\''
+  abbr --add --global  generate 'rm -f ~/dst/.files && ~/bin/ssg5 src dst "Ivar Castro" "http://ivarcastro.com"'
+end
